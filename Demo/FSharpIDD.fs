3aeca21ed8874ff2782b5d9314def221983b4739
Dmitry A. Grechka <dmitry@grechka.family>
2018-12-03 17:59:55 +0300

    Initial source drop

---
+++ Demo/FSharpIDD.fs
@@ -0,0 +1,168 @@
+﻿namespace FsharpIDD
+
+/// This module is to reduce the dependencies count and to ease possible WebSharper compilation
+module Colour =    
+    type Colour = {
+        R: byte
+        G: byte
+        B: byte
+    }
+
+    let createColour red green blue = {R=red; G=green;B=blue}
+
+    let Red = createColour (byte 255) (byte 0) (byte 0)
+    let Green = createColour (byte 0) (byte 255) (byte 0)
+    let Blue = createColour (byte 0) (byte 0) (byte 255)
+
+module Plot =
+    type DataSeries = float seq
+    
+    /// The single polyline settings
+    type Polyline = {
+        /// Specifies how to annotate the polyline in the legend
+        Name: string option
+        /// Series of X coords of the points that form the polyline
+        X: DataSeries
+        /// Series of Y coords of the points that form the polyline
+        Y: DataSeries
+        /// The fill colour of the polyline
+        Colour: Colour.Colour option
+        /// The line thickness of the polyline
+        Thickness: float option
+    }
+
+    /// Creates a basic polyline using the specifies set of X and Y coords
+    let createPolyline x y =    
+            {
+                X = x
+                Y = y
+                Colour = None
+                Thickness = None
+                Name = None
+            }
+    /// Changes the name of the polyline (how it is depicted in the legend)
+    let setLineName name polyline =
+        {
+            polyline with
+                Name = Some(name)
+        }
+
+    /// Changes the colour of the polyline
+    let setLineStrokeColour colour polyline =
+        {
+            polyline with
+                Colour = Some(colour)
+        }
+
+    let setLineThickness thickness polyline =
+        {
+            polyline with
+                Thickness = Some(thickness)
+        }
+
+    type Plot =
+    |   Polyline of Polyline
+
+    type SizeType = int
+
+module Chart =
+    open System.Xml
+    open System.IO
+    open System.Text
+
+    open Plot
+
+    /// Represents single chart that can be transformed later into the HTML IDD Chart
+    type Chart = {
+        /// The width and height of the chart in pixels
+        Size: (SizeType * SizeType) option // width * Height
+        /// The text that is centered and placed above the chart
+        Title: string option
+        Xlabel: string option
+        Ylabel: string Option
+        Plots: Plot list
+    }
+
+    let Empty : Chart = {
+        Size = None
+        Title = None
+        Xlabel = None
+        Ylabel = None
+        Plots = []
+    }    
+
+    let addPolyline polyline chart =
+        {
+            chart with
+                Plots = Polyline(polyline)::chart.Plots
+        }
+    
+
+    let toHTML (chart:Chart) = 
+        let root = XmlDocument()
+
+        let chartNode = root.CreateElement("div")
+        root.AppendChild(chartNode) |> ignore
+
+        let chartClassAttribure = root.CreateAttribute("class")
+        chartClassAttribure.AppendChild(root.CreateTextNode("fsharp-idd")) |> ignore
+        chartNode.Attributes.Append(chartClassAttribure) |> ignore
+
+        let chartPlotAttr = root.CreateAttribute("data-idd-plot")
+        chartPlotAttr.AppendChild(root.CreateTextNode("chart")) |> ignore
+        chartNode.Attributes.Append(chartPlotAttr) |> ignore
+    
+        let styleAttribute = root.CreateAttribute("style")
+        styleAttribute.AppendChild(root.CreateTextNode("width: 800px; height: 600px;")) |> ignore
+        chartNode.Attributes.Append(styleAttribute) |> ignore
+
+        let polylineToDom (p:Polyline) =
+            let getDataDom xSeries ySeries =
+                let builder = StringBuilder()
+                builder.AppendLine("\tx\ty") |> ignore
+                Seq.iter2 (fun x y -> builder.AppendLine(sprintf "\t%f\t%f" x y) |> ignore) xSeries ySeries
+                builder.ToString()
+
+            let resultNode = root.CreateElement("div")
+
+            let plotAttribute = root.CreateAttribute("data-idd-plot")
+            plotAttribute.AppendChild(root.CreateTextNode("polyline")) |> ignore
+            resultNode.Attributes.Append(plotAttribute) |> ignore
+
+            let plotStyleAttribute = root.CreateAttribute("data-idd-style")
+            let styleEntries = []
+            let styleEntries = 
+                match p.Thickness with
+                | Some(thickness) -> (sprintf "thickness: %.1f" thickness)::styleEntries
+                | None -> styleEntries
+            let styleEntries = 
+                match p.Colour with
+                | Some(c) -> (sprintf "stroke: rgb(%d,%d,%d)" c.R c.G c.B)::styleEntries
+                | None -> styleEntries            
+            plotStyleAttribute.AppendChild(root.CreateTextNode(System.String.Join("; ",styleEntries))) |> ignore
+            resultNode.Attributes.Append(plotStyleAttribute) |> ignore
+
+            match p.Name with
+            | Some(name) ->
+                let nameAttribute = root.CreateAttribute("data-idd-name")
+                nameAttribute.AppendChild(root.CreateTextNode(name)) |> ignore
+                resultNode.Attributes.Append(nameAttribute) |> ignore
+            | None -> ()
+
+            resultNode.InnerText <- (getDataDom p.X p.Y)
+
+            resultNode
+    
+        let plotToDom plot =
+            match plot with
+            |   Polyline p -> polylineToDom p
+
+        let plotElems = chart.Plots |> Seq.map plotToDom
+        plotElems |> Seq.iter (fun elem -> chartNode.AppendChild(elem) |> ignore)
+        use writer = new StringWriter()
+        use xmlWriter = new XmlTextWriter(writer)
+        root.WriteContentTo(xmlWriter)
+        let res = writer.ToString()
+        res
+
+    
---
+++ Demo/Program.fs
@@ -0,0 +1,46 @@
+﻿open FsharpIDD.Chart
+open FsharpIDD.Plot
+open System.IO
+open System.Diagnostics
+open FsharpIDD
+
+[<EntryPoint>]
+let main argv = 
+    // fake data generation
+    let Xseries = Array.init 1000 (fun i -> float(i)*0.01)
+
+    let Yseries = Xseries |> Array.map (fun x -> sin(2.0*x)/x)
+    let Yseries2 = Xseries |> Array.map (fun x -> sin(5.0*x)/x)
+        
+    // composing a chart
+    let curve1 = 
+        createPolyline Xseries Yseries
+        |> setLineName "Curve 1"
+        |> setLineStrokeColour Colour.Red
+        |> setLineThickness 2.0
+    
+    let curve2 =
+        createPolyline Xseries Yseries2
+        |> setLineStrokeColour Colour.Blue
+        |> setLineThickness 3.0
+        |> setLineName "Curve 2"
+
+    let chart =
+        Chart.Empty
+        |> Chart.addPolyline curve1
+        |> Chart.addPolyline curve2
+
+    // getting HTML that represents the chart
+    let generatedChart = chart |> toHTML
+
+    let template = File.ReadAllText "template.html"
+
+    // Injecting it into the HTML template
+    let html = template.Replace("<%PLACEHOLDER%>",generatedChart)
+    printfn "%s" generatedChart
+    let writer = File.CreateText("chart.html")
+    writer.Write(html)
+    writer.Close()
+    // showing the result HTML with browser
+    Process.Start("chart.html") |> ignore
+    0
