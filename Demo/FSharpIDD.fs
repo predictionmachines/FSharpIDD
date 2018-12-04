51c51118087e3f248090cc8fe8796fec58bbd0e6
Dmitry A. Grechka <dmitry@grechka.family>
2018-12-04 14:31:36 +0300

    multiple ways to specify the polyline options

--- Demo/FSharpIDD.fs
+++ Demo/FSharpIDD.fs
@@ -14,54 +14,87 @@ module Colour =
     let Green = createColour (byte 0) (byte 255) (byte 0)
     let Blue = createColour (byte 0) (byte 0) (byte 255)
 
-module Plot =
+module Plot =            
     type DataSeries = float seq
+
+    module Polyline =
+        /// The single polyline settings
+        type Plot = {
+            /// Specifies how to annotate the polyline in the legend
+            Name: string option
+            /// Series of X coords of the points that form the polyline
+            X: DataSeries
+            /// Series of Y coords of the points that form the polyline
+            Y: DataSeries
+            /// The fill colour of the polyline
+            Colour: Colour.Colour option
+            /// The line thickness of the polyline
+            Thickness: float option
+        }
+
+        type Options() =
+            let mutable name: string option = None
+            let mutable colour: Colour.Colour option = None
+            let mutable thickness: float option = None
+
+            member s.Name with set v = name <- Some(v)                
+            member s.SpecifiedName with internal get() = name
+            member s.Colour with set v = colour <- Some(v)
+            member s.SpecifiedColour with internal get() = colour
+            member s.Thickness with set v = thickness <- Some(v)
+            member s.SpecifiedThickness with internal get() = thickness
+                
     
-    /// The single polyline settings
-    type Polyline = {
-        /// Specifies how to annotate the polyline in the legend
-        Name: string option
-        /// Series of X coords of the points that form the polyline
-        X: DataSeries
-        /// Series of Y coords of the points that form the polyline
-        Y: DataSeries
-        /// The fill colour of the polyline
-        Colour: Colour.Colour option
-        /// The line thickness of the polyline
-        Thickness: float option
-    }
+        /// sets several polyline options at once
+        let setOptions (options:Options) polyline =
+            let polyline =
+                match options.SpecifiedName with
+                | None -> polyline
+                | Some(name) -> {polyline with Name = Some(name)}
+            let polyline =
+                match options.SpecifiedColour with
+                | None -> polyline
+                | Some(color) -> {polyline with Colour = Some(color)}
+            let polyline =
+                match options.SpecifiedThickness with
+                | None -> polyline
+                | Some(thickness) -> {polyline with Thickness = Some(thickness)}
+            polyline
+        
 
-    /// Creates a basic polyline using the specifies set of X and Y coords
-    let createPolyline x y =    
+
+        /// Creates a basic polyline using the specifies set of X and Y coords
+        let createPolyline x y =    
+                {
+                    X = x
+                    Y = y
+                    Colour = None
+                    Thickness = None
+                    Name = None
+                }
+        /// Changes the name of the polyline (how it is depicted in the legend)
+        let setName name polyline =
             {
-                X = x
-                Y = y
-                Colour = None
-                Thickness = None
-                Name = None
+                polyline with
+                    Name = Some(name)
             }
-    /// Changes the name of the polyline (how it is depicted in the legend)
-    let setLineName name polyline =
-        {
-            polyline with
-                Name = Some(name)
-        }
 
-    /// Changes the colour of the polyline
-    let setLineStrokeColour colour polyline =
-        {
-            polyline with
-                Colour = Some(colour)
-        }
+        /// Changes the colour of the polyline
+        let setStrokeColour colour polyline =
+            {
+                polyline with
+                    Colour = Some(colour)
+            }
 
-    let setLineThickness thickness polyline =
-        {
-            polyline with
-                Thickness = Some(thickness)
-        }
+        let setThickness thickness polyline =
+            {
+                polyline with
+                    Thickness = Some(thickness)
+            }
+
 
     type Plot =
-    |   Polyline of Polyline
+    |   Polyline of Polyline.Plot
 
     type SizeType = int
 
@@ -116,7 +149,7 @@ module Chart =
         styleAttribute.AppendChild(root.CreateTextNode("width: 800px; height: 600px;")) |> ignore
         chartNode.Attributes.Append(styleAttribute) |> ignore
 
-        let polylineToDom (p:Polyline) =
+        let polylineToDom (p:Polyline.Plot) =
             let getDataDom xSeries ySeries =
                 let builder = StringBuilder()
                 builder.AppendLine("\tx\ty") |> ignore
--- Demo/Program.fs
+++ Demo/Program.fs
@@ -1,34 +1,46 @@
 ﻿open FsharpIDD.Chart
 open FsharpIDD.Plot
+open FsharpIDD.Plot.Polyline
 open System.IO
 open System.Diagnostics
 open FsharpIDD
+open System.Security
 
 [<EntryPoint>]
 let main argv = 
     // fake data generation
-    let Xseries = Array.init 1000 (fun i -> float(i)*0.01)
+    let Xseries = Array.init 1000 (fun i -> float(i+1)*0.01)
 
-    let Yseries = Xseries |> Array.map (fun x -> sin(2.0*x)/x)
+    let Yseries1 = Xseries |> Array.map (fun x -> sin(2.0*x)/x)
     let Yseries2 = Xseries |> Array.map (fun x -> sin(5.0*x)/x)
-        
-    // composing a chart
-    let curve1 = 
-        createPolyline Xseries Yseries
-        |> setLineName "Curve 1"
-        |> setLineStrokeColour Colour.Red
-        |> setLineThickness 2.0
+    let Yseries3 = Xseries |> Array.map (fun x -> sin(5.0*x+1.0)/x)           
+
+    // Specifying some of the polyline options with setOption call
+    let curve1 =
+        createPolyline Xseries Yseries1
+        |> setOptions (Options(Name = "Curve 1", Thickness = 3.0))
     
-    let curve2 =
+    // Specifying some of the polyline options with a series of set... calls
+    let curve2 = 
         createPolyline Xseries Yseries2
-        |> setLineStrokeColour Colour.Blue
-        |> setLineThickness 3.0
-        |> setLineName "Curve 2"
+        |> setName "Curve 2"
+        |> setStrokeColour Colour.Green
+        |> setThickness 2.0
+    
+    // Specifying some of the polyline options with a record recreation
+    let curve3 = createPolyline Xseries Yseries3
+    let curve3 =
+        {
+             curve3 with
+                Name = Some("Curve 3")
+                Thickness = Some(1.0)
+        }
 
     let chart =
         Chart.Empty
         |> Chart.addPolyline curve1
         |> Chart.addPolyline curve2
+        |> Chart.addPolyline curve3
 
     // getting HTML that represents the chart
     let generatedChart = chart |> toHTML
