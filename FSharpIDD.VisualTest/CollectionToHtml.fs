e535a213031b775dfe4bc128205fc7fe8c52a783
LucyMu <lucymu@redshiftlab.ru>
2018-12-10 12:00:04 +0300

    added VisualTests project

---
+++ FSharpIDD.VisualTest/CollectionToHtml.fs
@@ -0,0 +1,34 @@
+﻿module CollectionToHtml
+
+open System.Xml
+open System.IO
+
+let toHTML (sampleList: (string*string)list) = 
+    let root = XmlDocument()
+
+    let samplesNode = root.CreateElement("div")
+    root.AppendChild(samplesNode) |> ignore
+
+    let arrangeSample sample =
+        let (chartName: string, chartSample: string) = sample
+
+        let sampleNode = root.CreateElement("div")
+
+        let chartNameDiv = root.CreateElement("div")
+        chartNameDiv.InnerText <- chartName
+        chartNameDiv.SetAttribute("style", "font-weight: bold")
+        sampleNode.AppendChild(chartNameDiv) |> ignore
+
+        let chartDiv = root.CreateDocumentFragment()
+        chartDiv.InnerXml <- chartSample
+        sampleNode.AppendChild(chartDiv) |> ignore
+
+        samplesNode.AppendChild(sampleNode) |> ignore
+
+    sampleList |> Seq.iter arrangeSample
+        
+    use writer = new StringWriter()
+    use xmlWriter = new XmlTextWriter(writer)
+    root.WriteContentTo(xmlWriter)
+    let res = writer.ToString()
+    res
---
+++ FSharpIDD.VisualTest/Program.fs
@@ -0,0 +1,73 @@
+﻿open System.IO
+open System.Diagnostics
+open FSharpIDD
+open CollectionToHtml
+open FSharpIDD.Plot
+open FSharpIDD.Chart
+
+[<EntryPoint>]
+let main argv =
+
+    let template = File.ReadAllText "template.html"
+
+    let Xseries = Array.init 3 (fun i -> float(i+1))
+    let Yseries1 = Xseries |> Array.map (fun x -> sin(2.0*x)/x)
+    
+    let redColour2ThickRedNameCurve : Polyline.Plot =
+        {
+            X = Xseries
+            Y = Yseries1
+            Name = "Red"
+            Colour = Colour.Red
+            Thickness = 2.0
+        }
+
+
+
+    // Empty chart
+    let emptyChartStr = Chart.Empty |> toHTML
+    let emptyChartTest = "1. Empty chart", emptyChartStr
+
+    // Preset chart
+    let presetChart = Chart.addPolyline redColour2ThickRedNameCurve Chart.Empty
+    let presetChartStr = presetChart |> toHTML
+    let presetChartTest = "2. This is a chart which properies are overridden in the next tests", presetChartStr
+    
+
+    // Specifying empty set of the polyline options with setOption call
+    let noPropertiesSetCurve = Polyline.setOptions (Polyline.Options()) redColour2ThickRedNameCurve
+    let noNewPropertiesSetChart = Chart.addPolyline noPropertiesSetCurve Chart.Empty
+    let noNewPropertiesSetChartStr = noNewPropertiesSetChart |> toHTML
+    let setNoneTest = "3. Setting empty properties list to the initial polyline (2) with: Polyline.setOptions (Polyline.Options())", noNewPropertiesSetChartStr
+
+
+    // Specifying full set of the polyline options with setOption call
+    let greenColour6ThickGreenNameCurve = Polyline.setOptions (Polyline.Options(Name = "Green", Colour = Colour.Green, Thickness = 6.0)) redColour2ThickRedNameCurve
+    let allNewPropertiesSetChart = Chart.addPolyline greenColour6ThickGreenNameCurve Chart.Empty
+    let allNewPropertiesSetChartStr = allNewPropertiesSetChart |> toHTML
+    let setAllTest = "4. Overriding all properties of the polyline (2) with: Polyline.setOptions (Polyline.Options(Name = 'Green', Colour = Colour.Green, Thickness = 6.0))", allNewPropertiesSetChartStr
+
+    //let setStrokeTest = "Set stroke", Chart.Empty
+    //let setNameTest = "Set polyline name", Chart.Empty
+    //let setThicknessTest = "Set polyline thickness", Chart.Empty
+    
+
+    let tests = 
+        [
+            emptyChartTest
+            presetChartTest
+            setNoneTest
+            setAllTest
+        ]    
+
+
+    let generatedDiv = tests |> CollectionToHtml.toHTML 
+    
+    let html = template.Replace("<%PLACEHOLDER%>", generatedDiv)
+    printfn "%s" generatedDiv
+    let writer = File.CreateText("visualTests.html")
+    writer.Write(html)
+    writer.Close()
+    Process.Start("visualTests.html") |> ignore
+
+    0
