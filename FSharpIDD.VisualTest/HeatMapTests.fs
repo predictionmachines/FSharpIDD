beea2fdad97f6f2cb8e0cbdbf4c102b84fe33b5d
LucyMu <lucymu@redshiftlab.ru>
2018-12-21 20:11:21 +0300

    updated correlation sample

--- FSharpIDD.VisualTest/HeatMapTests.fs
+++ FSharpIDD.VisualTest/HeatMapTests.fs
@@ -41,27 +41,26 @@ let basicDescreteHeatmapTest =
 
     
 let corrMapX = Array.init<float> 7 (fun i -> float(i))
-let corrMapY = Array.init<float> 7 (fun i -> float(i))
 
-let random = System.Random(1)
-
-let corrMapVals = Array2D.init 6 6 (fun x y -> let x = 5-x in if y>=x then nan else random.NextDouble()*2.0-1.0)
+let corrMapVals = Array2D.init 6 6 (fun x y -> let y = 5-y in if x >= y then nan else (1.0 - 0.4 * float(abs(y - x))))
 
 let ticks = Array.init 6 (fun idx -> 0.5+float(idx))
 let labels = ["a"; "b"; "c"; "d"; "e"; "f"]
+let labelsRev = List.rev labels
     
 
 let corrMap =
-    Heatmap.createHeatmap corrMapX corrMapY corrMapVals
+    Heatmap.createHeatmap corrMapX corrMapX corrMapVals
     |> Heatmap.setPalette (Heatmap.Palette.IddPaletteString "-1.0=blue,white,red=1.0")
 
 let corrMapTest =
     "Correlation map between 6 species",
     [    
-        ""
+        "|> Heatmap.setPalette (Heatmap.Palette.IddPaletteString '-1.0=blue,white,red=1.0')"
+        "Correlation map"
     ],
     Empty
     |> Chart.setSize 600 400
     |> Chart.addHeatmap corrMap 
-    |> Chart.setXaxis (Labelled(ticks,labels))
-    |> Chart.setYaxis (Labelled(ticks,labels))
+    |> Chart.setXaxis (Labelled(ticks, labels))
+    |> Chart.setYaxis (Labelled(ticks, labelsRev))
